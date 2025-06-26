bdf25d3b317999c65ae2ef99dcdbb60c4aefda42
Nominy <sapkovich.vladislav@gmail.com>
2025-06-26 20:38:41 +0300

    initial commit

---
+++ Source/Starbreeze/Private/SBZAICharacterAbilityData.cpp
@@ -0,0 +1,11 @@
+#include "SBZAICharacterAbilityData.h"
+
+USBZAICharacterAbilityData::USBZAICharacterAbilityData() {
+    this->ArmorHardness = 0.00f;
+    this->ArmorDifficultyScaleArray[0] = 1.00f;
+    this->ArmorDifficultyScaleArray[1] = 1.00f;
+    this->ArmorDifficultyScaleArray[2] = 1.00f;
+    this->ArmorDifficultyScaleArray[3] = 1.00f;
+}
+
+
---
+++ Source/Starbreeze/Private/SBZStateMachineDataMatchmaking.cpp
@@ -0,0 +1,12 @@
+#include "SBZStateMachineDataMatchmaking.h"
+
+USBZStateMachineDataMatchmaking::USBZStateMachineDataMatchmaking() {
+    this->bAsyncLoadingComplete = false;
+    this->bCrossPlayEnabled = true;
+    this->bInCrossPlayLobby = false;
+    this->bCrossPlayAllowed = true;
+    this->JoinType = ESBZOnlineJoinType::Debug_DEPRECATED;
+    this->bIsRestart = false;
+}
+
+
